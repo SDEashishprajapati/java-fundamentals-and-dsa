974c70706596f2287ca784a9d961b87629e9e4cf
SDEashishprajapati <ashishprajapati4078@gmail.com>
2025-05-29 21:21:12 +0530

    Add basic Java programs

---
+++ JavaBasics01/AreaOfCircle.java
@@ -0,0 +1,14 @@
+package JavaBasics01;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+        double area = 3.14159 * radius * radius;
+        System.out.println("Area of circle: " + area);
+    }
+}
+
---
+++ JavaBasics01/PrintName.java
@@ -0,0 +1,13 @@
+package JavaBasics01;
+
+import java.util.Scanner;
+
+public class PrintName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Welcome, " + name + "!");
+    }
+}
+
