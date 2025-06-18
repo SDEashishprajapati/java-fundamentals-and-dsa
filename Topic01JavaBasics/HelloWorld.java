ad44db91bc5b7f92970f8d945eaa94be492ba8d5
SDEashishprajapati <ashishprajapati4078@gmail.com>
2025-06-18 21:30:57 +0530

    Restore folder Topic01Javabasics

---
+++ Topic01JavaBasics/HelloWorld.java
@@ -0,0 +1,6 @@
+package Topic01JavaBasics;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Topic01JavaBasics/PrintYourAge.java
@@ -0,0 +1,8 @@
+package Topic01JavaBasics;
+
+public class PrintYourAge {
+    public static void main(String[] args) {
+        int age = 22;
+        System.out.println("My age is " + age);
+    }
+}
